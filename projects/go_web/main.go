46a749050de9310493ad5b938aaac163d3d3ed36
daniel.barragan <daniel.barragan@geniussports.com>
2023-06-05 00:14:25 -0500

    feat: Add python and go examples

---
+++ projects/go_web/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/kriscfoster/multi-language-bazel-monorepo/projects/go_hello_world"
+)
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request")
+	w.Write([]byte(go_hello_world.HelloWorld()))
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", YourHandler)
+
+	log.Println("Going to listen on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
